900e187df68b5818370f9608150c2cbce1cd416b
Daniela <97981090+DaniBarrios22@users.noreply.github.com>
2022-08-15 12:30:56 -0300

    Update ej24.sql

--- ej24.sql
+++ ej24.sql
@@ -1,4 +1,16 @@
-CREATE DATABASE CursoNet
-USE CursoNet
+CREATE DATABASE CursoNet;
+USE CursoNet;
 
-CREATE TABLE Instructores()
+CREATE TABLE Instructores(
+  Id INT INENTITY(1,1) PRIMARY KEY,
+  Nombre VARCHAR(50) NOT NULL,
+  Apellido VARCHAR(50) NOT NULL
+);
+
+INSERT INTO Instructores VALUES('Ruben', 'Benegas')
+INSERT INTO Instructores VALUES('Eugenio', 'Serrano')
+INSERT INTO Instructores VALUES('Joaquin', 'Mateos')
+INSERT INTO Instructores VALUES('Ezequiel', 'Etchecoin')
+INSERT INTO Instructores VALUES('Carlos', 'Cornejo')
+
+SELECT * FROM Instructores;
